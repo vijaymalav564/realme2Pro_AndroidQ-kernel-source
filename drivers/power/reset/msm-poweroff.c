309a8966dc7b93b1064d6e6c004b36e3bbc0d81a
Ethan Chen <intervigil@gmail.com>
2015-02-27 18:31:48 -0800

    power: reset: allow device to preserve memory on restart
    * Always perform a soft reset as this preserves memory contents,
      including pstore and other persistent memory.
    
    Change-Id: Ideca44c9f38d2ebcd437ff289f8e036922eafcd2

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -333,6 +333,27 @@ static void msm_restart_prepare(const char *cmd)
 		need_warm_reset = (get_dload_mode() ||
 				(cmd != NULL && cmd[0] != '\0'));
 	}
+#ifdef CONFIG_PRODUCT_REALME_RMX1801 
+//Fanhong.Kong@PSW.BSP.CHG,add 2018/3/25 panic reboot reason for kernel 
+	if (in_panic){
+		//warm reset
+		qpnp_pon_system_pwr_off(PON_POWER_OFF_WARM_RESET);
+		qpnp_pon_set_restart_reason(
+					PON_RESTART_REASON_KERNEL);
+		flush_cache_all();
+
+		/*outer_flush_all is not supported by 64bit kernel*/
+#ifndef CONFIG_ARM64
+		outer_flush_all();
+#endif
+		return;
+	}
+#endif /* CONFIG_PRODUCT_REALME_RMX1801 */
+
+#ifdef CONFIG_QCOM_PRESERVE_MEM
+	need_warm_reset = true;
+#endif
+
 	/* Hard reset the PMIC unless memory contents must be maintained. */
 	if (need_warm_reset) {
 		qpnp_pon_system_pwr_off(PON_POWER_OFF_WARM_RESET);

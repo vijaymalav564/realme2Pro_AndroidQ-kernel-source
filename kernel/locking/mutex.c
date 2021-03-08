168b782ae6ec7304ca0ee825b960cad2e3fd8efe
vijaymalav564 <jaymalav10@gmail.com>
2021-03-08 06:57:14 +0000

    Import Realme kernel changes
    Signed-off-by: vijaymalav564 <jaymalav10@gmail.com>

--- kernel/locking/mutex.c
+++ kernel/locking/mutex.c
@@ -576,7 +576,10 @@ __mutex_lock_common(struct mutex *lock, long state, unsigned int subclass,
 		 * got a signal? (This code gets eliminated in the
 		 * TASK_UNINTERRUPTIBLE case.)
 		 */
-		if (unlikely(signal_pending_state(state, task))) {
+		//#ifdef CONFIG_PRODUCT_REALME_RMX1801 fangpan@Swdp.shanghai,2015/11/12
+		if (unlikely(signal_pending_state(state, task))
+			|| hung_long_and_fatal_signal_pending(task)) {
+		//#endif
 			ret = -EINTR;
 			goto err;
 		}

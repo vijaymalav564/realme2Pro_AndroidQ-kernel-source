3fefea0775297969e67332c717aa97dbafcf8472
vijaymalav564 <jaymalav10@gmail.com>
2021-03-24 17:43:39 +0000

    Import Realme net changes
    Signed-off-by: vijaymalav564 <jaymalav10@gmail.com>

--- net/ipv4/netfilter/ipt_REJECT.c
+++ net/ipv4/netfilter/ipt_REJECT.c
@@ -30,6 +30,13 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Netfilter Core Team <coreteam@netfilter.org>");
 MODULE_DESCRIPTION("Xtables: packet \"rejection\" target for IPv4");
 
+#ifdef CONFIG_PRODUCT_REALME_RMX1801
+//Junyuan.Huang@PSW.CN.WiFi.Network.internet.1197891, 2018/04/10,
+//Add code for appo sla function
+void (*mark_streams_for_iptables_reject)(struct sk_buff *skb,enum ipt_reject_with reject_type) = NULL;
+EXPORT_SYMBOL(mark_streams_for_iptables_reject);
+#endif /* CONFIG_PRODUCT_REALME_RMX1801 */
+
 static unsigned int
 reject_tg(struct sk_buff *skb, const struct xt_action_param *par)
 {
@@ -64,6 +71,13 @@ reject_tg(struct sk_buff *skb, const struct xt_action_param *par)
 		/* Doesn't happen. */
 		break;
 	}
+#ifdef CONFIG_PRODUCT_REALME_RMX1801
+//Junyuan.Huang@PSW.CN.WiFi.Network.internet.1197891, 2018/04/10,
+//Add code for appo sla function	for mark streams deal with iptables reject
+	if (mark_streams_for_iptables_reject) {
+		mark_streams_for_iptables_reject(skb,reject->with);
+	}
+#endif /* CONFIG_PRODUCT_REALME_RMX1801 */
 
 	return NF_DROP;
 }

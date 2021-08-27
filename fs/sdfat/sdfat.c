fda9d32c79f5f45ed6ce7c5d6f147ab40ffa40e7
vijaymalav564 <jaymalav10@gmail.com>
2021-08-27 06:10:36 +0000

    Revert "fs: sdfat: enable discard by default"
    This reverts commit fb1d028bdd1ffd337668f371b9c13b0d9c1f8c3c.

--- fs/sdfat/sdfat.c
+++ fs/sdfat/sdfat.c
@@ -4458,7 +4458,7 @@ static int __sdfat_show_options(struct seq_file *m, struct super_block *sb)
 		seq_puts(m, ",errors=panic");
 	else
 		seq_puts(m, ",errors=remount-ro");
-//	if (opts->discard)
+	if (opts->discard)
 		seq_puts(m, ",discard");
 
 	return 0;
@@ -4715,7 +4715,7 @@ static int parse_options(struct super_block *sb, char *options, int silent,
 	opts->amap_opt.misaligned_sect = 0;
 	opts->symlink = 0;
 	opts->errors = SDFAT_ERRORS_RO;
-	opts->discard = 1;
+	opts->discard = 0;
 	*debug = 0;
 
 	if (!options)
@@ -4885,12 +4885,11 @@ out:
 	if (opts->discard) {
 		struct request_queue *q = bdev_get_queue(sb->s_bdev);
 
-		if (!blk_queue_discard(q)) {
+		if (!blk_queue_discard(q))
 			sdfat_msg(sb, KERN_WARNING,
 				"mounting with \"discard\" option, but "
 				"the device does not support discard");
-			opts->discard = 0;
-		}
+		opts->discard = 0;
 	}
 
 	return 0;
